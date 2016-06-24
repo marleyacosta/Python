d43f687777f79e8574dcbc3cb722ea9c977fb851
marleyacosta <marleywhite4@gmail.com>
2016-06-23 20:26:33 -0400

    Update UnlabeledTrees.py

---
+++ UnlabeledTrees.py
@@ -0,0 +1,28 @@
+
+def leaves(degree, l):
+    for i in range(degree):
+        l.append(1)
+        
+def unlabeledTrees(vertices):
+    numberOfEdges = vertices - 1
+    degreeRange = range(1,numberOfEdges + 1)
+    degreeSequence = []
+    
+    for highestDegree in degreeRange:
+        
+        degreeSequence.append(highestDegree)
+        
+        leaves(highestDegree,degreeSequence)
+        
+        degreeSum = 2*(numberOfEdges)
+        
+        k = 2*(highestDegree)
+        
+        degreeSum -= k
+        
+        unknownVertices = vertices - (highestDegree - 1)
+        
+        print(degreeSequence)
+        
+unlabeledTrees(7)
+        
