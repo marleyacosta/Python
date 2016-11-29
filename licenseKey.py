fc2289e2da2a9646b93fc6014568bf48e8c58be2
Maurely Acosta <marleywhite4@gmail.com>
2016-11-29 10:46:21 -0500

    adding solution method
    adding solution method

--- licenseKey.py
+++ licenseKey.py
@@ -1,1 +1,16 @@
-# Given a license key such as "2-4A0r7-4k". Add dashes every k character to create groups. The first group can have a smaller number of charcters
+# Given a license key such as "2-4A0r7-4k".
+# Add dashes every K character to create groups.
+# The first group can have a smaller number of characters
+
+def solution(licenseKey, K):
+  licenseKey = licenseKey.replace("-","") # Removes the dashes
+  S = list(licenseKey.upper()) # convert string to uppercase
+  
+  #Traverse backwards by K step starting at 0
+  for i in range( len(licenseKey) - K, 0, -K ):
+     licenseKey.insert(i, "-")
+      
+  licenseKey = "".join(licenseKey)
+  
+  return licenseKey
+
